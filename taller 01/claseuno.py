405cca3b11f70f31ea2d7485078f5f2ba076bddc
jose ricardo <planeacionrte@gmail.com>
2020-06-13 11:05:36 -0500

    programa pits

---
+++ taller 01/claseuno.py
@@ -0,0 +1,13 @@
+print("proyecto pit")
+print("bienvenido al simulador de parada de pits")
+wells_Number=4
+Wells_Change=input(int("number of weels to change"))
+Time_goal=6
+carro1="entta a pits"
+mechanic=time1
+mechanic2=time2
+mechanic1=time3
+mechanic3=time4
+mechanic4=time5
+mechanic5=time6
+
