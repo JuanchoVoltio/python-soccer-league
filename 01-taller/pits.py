430611bba82c8ac3b05eec89677ce746012c1aee
Daniel Rubiano <dcrubiano@gmail.com>
2020-06-13 11:28:52 -0500

    Simulacion parada pits

--- 01-taller/pits.py
+++ 01-taller/pits.py
@@ -1,3 +1,21 @@
-pits = False
-if pits != True:
-    print ("No ha entrado a pits")
+#Programa de simulación de pits para indicar
+#Definir las variables
+tires = 4
+
+#Simular tiempos para cada mecanico
+time_crew_member_1 = 5
+time_crew_member_2 = 3.3
+time_crew_member_3 = 2.4
+time_crew_member_4 = 4.2
+
+#Mostrar el resultado del trabajo de pits
+#1.Tiempos de cada mecanico
+#2.Mostrar tiempo total de la parada de pits
+print(time_crew_member_1)
+print(time_crew_member_2)
+print(time_crew_member_3)
+print(time_crew_member_4)
+
+total = time_crew_member_1 + time_crew_member_2 + time_crew_member_3 + time_crew_member_4
+print("El tiempo total en pits en segundos fue: ")
+print(total)
