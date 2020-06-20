f050b0cdc63bbcd309ce5a36fc882bde6a3e3b6a
Daniel Rubiano <dcrubiano@gmail.com>
2020-06-20 09:57:48 -0500

    Mejora pits

--- 01-taller/pits.py
+++ 01-taller/pits.py
@@ -12,11 +12,7 @@ time_crew_member_4 = round(random.uniform(0.1,10),2)
 #Mostrar el resultado del trabajo de pits
 #1.Tiempos de cada mecanico
 #2.Mostrar tiempo total de la parada de pits
-print(time_crew_member_1)
-print(time_crew_member_2)
-print(time_crew_member_3)
-print(time_crew_member_4)
+print("El tiempo del mecanico 1 fue {0}s, el 2 fue {1}s, el 3 fue{2}s, el 4 fue{3}".format(time_crew_member_1, time_crew_member_2, time_crew_member_3, time_crew_member_4))
 
 total = max([time_crew_member_1, time_crew_member_2, time_crew_member_3, time_crew_member_4])
-print("El tiempo total en pits fue: ")
-print(str(total) + 's')
+print("El tiempo total en pits fue: {0} s".format(total))
