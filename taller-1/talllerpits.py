2eacaa46b81f00ce3e8c3758f9c4a4b924837b57
edgar <edgarrojasu@@hotmail.com>
2020-06-19 19:07:31 -0500

    talle pits

---
+++ taller-1/talllerpits.py
@@ -0,0 +1,20 @@
+tires = 4
+
+#tiempos de cambio de cada llanta
+time_change_1 = 4
+time_change_2 = 2.3
+time_change_3 = 3.1
+time_change_4 = 4.3
+
+#Para mostrar el tiempo de cada cambio de llanta se utiliza la fincion print
+
+#2.Mostrar tiempo total de la parada de pits
+print("El siguiente programa muesta la duracion de un carro en pits")
+print("El tiempo en cambiar la llanta 1 fue: ",time_change_1, "segundos")
+print("El tiempo en cambiar la llanta 2 fue: ",time_change_2, "segundos")
+print("El tiempo en cambiar la llanta 3 fue: ",time_change_3, "segundos")
+print("El tiempo en cambiar la llanta 4 fue: ",time_change_4, "segundos")
+#realiza la comparacion con la funcion if entre los 4 tiempos y verifica cual es el tiempo mayor
+
+tiempo_pits = time_change_4 
+print("El tiempo en pits fue: ", tiempo_pits, "segundos")
