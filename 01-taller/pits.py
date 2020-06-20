62c2e2bdb1a1686aa991ad23f6e22d800a1769c9
Daniel Rubiano <dcrubiano@gmail.com>
2020-06-20 09:46:15 -0500

    Mejora ejercicios pits

--- 01-taller/pits.py
+++ 01-taller/pits.py
@@ -1,12 +1,13 @@
+import random 
 #Programa de simulación de pits para indicar
 #Definir las variables
 tires = 4
 
 #Simular tiempos para cada mecanico
-time_crew_member_1 = 5
-time_crew_member_2 = 3.3
-time_crew_member_3 = 2.4
-time_crew_member_4 = 4.2
+time_crew_member_1 = round(random.uniform(0.1,10),2)
+time_crew_member_2 = round(random.uniform(0.1,10),2)
+time_crew_member_3 = round(random.uniform(0.1,10),2)
+time_crew_member_4 = round(random.uniform(0.1,10),2)
 
 #Mostrar el resultado del trabajo de pits
 #1.Tiempos de cada mecanico
@@ -16,6 +17,6 @@ print(time_crew_member_2)
 print(time_crew_member_3)
 print(time_crew_member_4)
 
-total = time_crew_member_1 + time_crew_member_2 + time_crew_member_3 + time_crew_member_4
-print("El tiempo total en pits en segundos fue: ")
-print(total)
+total = max([time_crew_member_1, time_crew_member_2, time_crew_member_3, time_crew_member_4])
+print("El tiempo total en pits fue: ")
+print(str(total) + 's')
