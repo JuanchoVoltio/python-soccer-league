90b8bfde59b9ab075b8a10e628d2bc40f929c9f5
LuisCarlosVanegas <66173444+LuisCarlosVanegas@users.noreply.github.com>
2020-06-20 09:54:05 -0500

    Add files via upload

---
+++ TALLER1a/pitsluis.py
@@ -0,0 +1,42 @@
+Python 2.7.5 (default, May 15 2013, 22:43:36) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Taller clase 3 variables y operaciones en python
+>>> #Formula 1 con dos mecanico con rueda
+>>> #Conocer tiempo en pits
+>>> #Asignación de variables llanta 1: m1= mecanico 1 y  m2 = mecanico 2 por rueda
+>>> llanta1m1 = 2
+>>> llanta1m2 = 3
+>>> #Calculo del tiempo en segundo demorado en pits llanta 1
+>>> tiempollanta1 = llanta1m1 + llanta1m2
+>>> tiempollanta1
+5
+>>> #Asignación de variables llanta 2: m1= mecanico 1 y  m2 = mecanico 2 por rueda
+>>> llanta2m1 = 3
+>>> llanta2m2 = 2
+>>> #Calculo del tiempo en segundo demorado en pits llanta 2 con su respectiva asignación de variable
+>>> tiempollanta2 = llanta2m1 + llanta2m2
+>>> tiempollanta2
+5
+>>> #Asignación de variables llanta 3: m1= mecanico 1 y  m2 = mecanico 2 por rueda
+>>> llanta3m1 = 2
+>>> llanta3m2 = 2
+>>> #Calculo del tiempo en segundo demorado en pits llanta 3 con su respectiva asignación de variable
+>>> tiempollanta3 = llanta3m1 + llanta3m2
+>>> tiempollanta3
+4
+>>> #Asignación de variables llanta 4: m1= mecanico 1 y  m2 = mecanico 2 por rueda
+>>> llanta4m1 = 4
+>>> llanta4m2 = 2
+>>> #Calculo del tiempo en segundo demorado en pits llanta 4 con su respectiva asignación de variable
+>>> tiempollanta4 = llanta4m1 + llanta4m2
+>>> tiempollanta4
+6
+>>> #Calculo del tiempo total en pits para las 4 llantas y asignaciòn de variables
+>>> tiempo = tiempollanta1 + tiempollanta2 + tiempollanta3 + tiempollanta4
+>>> tiempo
+20
+>>> #Mostrar en pantalla tiempo total en segundos en pits para las 4 llantas
+>>> print("El tiempo total en segundo en pits es:"); tiempo
+El tiempo total en segundo en pits es:
+20
+>>> 
