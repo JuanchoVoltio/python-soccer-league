977a4679129d3e67f11faa5bb43cfa9115dfcfd3
Henry L. Gómez <henryl.gomez@gmail.com>
2020-06-27 09:44:40 -0500

    Observaciones sobre el código

--- taller 2/clase2b.py
+++ taller 2/clase2b.py
@@ -1,3 +1,6 @@
-mensaje=input("escriba un mensaje")
+mensaje = input("escriba un mensaje")
+
+#obtener longitud_mensaje
+
 if mensaje < 3:
-print(mensaje[2:3:-1])
+print(mensaje[2:3:-1]) #Tabular el print para asociarlo al if
