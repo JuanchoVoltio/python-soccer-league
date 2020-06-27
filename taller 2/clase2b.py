e3313273ad67ec6296db0766b0cf3ddffb881794
jose ricardo <planeacionrte@gmail.com>
2020-06-27 09:27:47 -0500

    taller2b

---
+++ taller 2/clase2b.py
@@ -0,0 +1,3 @@
+mensaje=input("escriba un mensaje")
+if mensaje < 3:
+print(mensaje[2:3:-1])
---
+++ taller 2/condicionales.py
@@ -0,0 +1,6 @@
+print ("sacar una cadena de caracteres de un str")
+
+x="programoenpythonmediviertoalaprenderperosinpausaysinprisa"
+result= x[x.in("di"):x.index("al")]
+print(result)
+
