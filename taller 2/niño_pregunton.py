e4ebddfa46f5b473709b993bf8c991fd34e09be0
jose ricardo <planeacionrte@gmail.com>
2020-07-04 09:37:31 -0500

    while

---
+++ taller 2/niño_pregunton.py
@@ -0,0 +1,9 @@
+print("progrma para niño que hace preguntas")
+mensaje="introduzca la pregunta del niño"
+niño_pregunta= str(input(mensaje))
+
+while niño_pregunta=mensaje:
+    if menasaje ==True
+    print("la respuesta es")
+else:
+    print("otra pregunta")
---
+++ taller 2/whilenino.py
@@ -0,0 +1,9 @@
+print("progrma para niño que hace preguntas")
+mensaje="introduzca la pregunta del niño"
+niño_pregunta= input(mensaje)
+
+while niño_pregunta==mensaje:
+    if mensaje ==True:
+        print("la respuesta es")
+else:
+    print("otra pregunta")
