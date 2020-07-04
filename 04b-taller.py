b43916d02daea0586de2bec65e761b71073670ec
Daniel Rubiano <dcrubiano@gmail.com>
2020-07-04 10:41:05 -0500

    Taller 04b

---
+++ 04b-taller.py
@@ -0,0 +1,27 @@
+#Figura 1
+#h=5
+#b=6
+#for i in range(0,h,1):
+#    print("*"*b)
+#==================================
+
+
+
+
+#Figura 2
+#h = 5
+
+#for i in range(0,1,1):
+#    for j in range(0,h,1):
+#        print("*" + "*"*j)
+#=================================
+
+
+
+#Figura 3
+h=4
+b=7
+
+for i in range(0,h,1):    
+    for j in range(b,1,-2):
+        print("*"*j)
