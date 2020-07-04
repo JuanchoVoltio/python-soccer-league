d280ff0e535740b4a716fbe9dc8c6a8a93913bbf
edgar <edgarrojasu@@hotmail.com>
2020-07-04 09:22:22 -0500

    niño pregunton

---
+++ taller4/niño_pregunton.py
@@ -0,0 +1,10 @@
+pregunta= input("realize una pregunta")
+respuesta= input("escriba la respuesta")
+while pregunta == true:
+  respuesta= input("escriba la respuesta")
+  if respuesta == "porque si":
+    print("ya no puedes preguntar mas")
+  else:
+     print("realice otra pregunta")
+    
+  
