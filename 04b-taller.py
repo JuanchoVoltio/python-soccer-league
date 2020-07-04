03b0369897f8b9b8e8a3801ca43e33e05dd708dc
Daniel Rubiano <dcrubiano@gmail.com>
2020-07-04 14:52:05 -0500

    Taller 04b COMPLETO

--- 04b-taller.py
+++ 04b-taller.py
@@ -1,27 +1,50 @@
 #Figura 1
-#h=5
-#b=6
-#for i in range(0,h,1):
-#    print("*"*b)
+"""
+h=5
+b=6
+for i in range(0,h,1):
+    print("*"*b)
+"""
 #==================================
 
 
 
 
 #Figura 2
-#h = 5
-
-#for i in range(0,1,1):
-#    for j in range(0,h,1):
-#        print("*" + "*"*j)
+"""
+h = 5
+for i in range(0,1,1):
+    for j in range(0,h,1):
+        print("*" + "*"*j)
+"""
 #=================================
 
 
 
 #Figura 3
-h=4
-b=7
+"""
 
-for i in range(0,h,1):    
-    for j in range(b,1,-2):
-        print("*"*j)
+b=7
+espacios=0
+
+for j in range(b,0,-2):
+    print(" "*espacios+ "*"*j + " "*espacios)
+    espacios += 1
+""" 
+#======================================
+
+
+
+#Figura 4
+
+b=6
+espacios=0
+
+for j in range(b,0,-2):
+    print(" "*espacios+ "*"*j + " "*espacios)
+    espacios += 1
+    if j == 2:
+        for i in range(0,b+1,2): 
+            print(" "*espacios + "*"*i + " "*espacios) 
+            espacios -= 1
+    
