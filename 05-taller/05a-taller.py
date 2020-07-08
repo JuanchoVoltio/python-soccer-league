b578324ebe0cefa876460808c7b2f4372fc0e619
Daniel Rubiano <dcrubiano@gmail.com>
2020-07-08 08:20:07 -0500

    taller 5a

--- 05-taller/05a-taller.py
+++ 05-taller/05a-taller.py
@@ -1,13 +1,32 @@
 #Fechas
-dates = {"06-02","07-02", "13-02", "14-02", "20-02", "21-02", "27-02", "28-02", "06-03","07-03", "13-03", "14-03", "20-03", "21-03", "27-03", "28-03"}
+dates = {"06-02","07-02", "13-02", "14-02", "20-02", "21-02", "27-02", "28-02", "05-03","06-03", "12-03", "13-03", "19-03", "20-03", "26-03", "27-03"}
+dias_ocupados_febrero = set()
+dias_ocupados_marzo = set()
+todos_los_dias = set()
 
 for x in dates:
     if "02" in x:
         y = x.split("-")
-        febrero = [int(y[0])]
-        #print(febrero)
-        for i in range(1,31,1):
-            if i not in febrero:
-                print (i)
-        
-    
+        dias_ocupados_febrero.add(int(y[0]))
+  
+for x in dates:
+    if "03" in x:
+        y = x.split("-")
+        dias_ocupados_marzo.add(int(y[0]))
+
+for i in range(1,31,1):
+    todos_los_dias.add(i)
+
+print("")
+
+print("=====FEBRERO=====")
+print("Las días disponibles para el mes de Febrero son: ")
+print(todos_los_dias.difference(dias_ocupados_febrero))
+
+print("")
+
+print("=====MARZO=====")
+print("Las días disponibles para el mes de Marzo son: ")
+print(todos_los_dias.difference(dias_ocupados_marzo))
+
+print("")
