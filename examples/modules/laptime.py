95a7c0e6bf9bd97d28811523efd4282c4831e280
Henry L. Gómez <henryl.gomez@gmail.com>
2020-08-22 08:12:59 -0500

    Add new method definition

--- examples/modules/laptime.py
+++ examples/modules/laptime.py
@@ -12,3 +12,8 @@ def get_formatted_laptime(seconds=0, milliseconds=0):
     return '%s' % (str(time_delta))
   
 get_formatted_laptime.__doc__ = "Recibe los segundos o millisegundos a convertir en formato hh:mm:ss.fff"
+
+def get_time_difference(in_seconds=False, in_millis=True, time_a, time_b):
+    pass
+
+get_formatted_laptime.__doc__ = "Retorna la diferencia de dos tiempos de vuelta en forma de milisegundos o segundos"
