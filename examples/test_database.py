0b751227ff28558afd121e54667a41366d88b667
Henry Luis <henryl.gomez@gmail.com>
2020-08-22 10:18:30 -0500

    Update test_database.py

--- examples/test_database.py
+++ examples/test_database.py
@@ -1,5 +1,9 @@
 import mysql.connector as db_connector
 
+perform_select = False
+perform_insert = True
+operations = ""
+
 try:
     mydb = db_connector.connect(
       host = "185.201.11.128",
@@ -9,30 +13,32 @@ try:
     )
 
     mycursor = mydb.cursor()
-    print(type(mycursor))
 
-    sql_raw = "INSERT INTO drivers (car_number, driver_name) VALUES (18, 'L. Stroll')"
-    sql = "INSERT INTO drivers (car_number, driver_name) VALUES (%s, %s)" #prepared statement
-    values = ("26", "D. Kvyat")
-    #mycursor.execute(sql_raw)
-    #mydb.commit()
-    print(mycursor.rowcount, "record inserted.")
+    if perform_insert:
+        print("Ejecutando un INSERT")
+        operations = "INSERT "
+        sql_raw = "INSERT INTO drivers (car_number, driver_name) VALUES (18, 'L. Stroll')"
+        #sql = "INSERT INTO drivers (car_number, driver_name) VALUES (%s, %s)" #prepared statement
+        #values = ("26", "D. Kvyat")
+        mycursor.execute(sql_raw)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
 
-    mycursor = mydb.cursor()
+    if perform_select:
+        print("Ejecutando un SELECT")
+        operations += "SELECT "
+        mycursor.execute("SELECT * FROM driverss")
+        myresult = mycursor.fetchall() #Obtener el resulSet de la consulta
 
-    mycursor.execute("SELECT * FROM drivers")
-
-    myresult = mycursor.fetchall()
-
-    for x in myresult:
-      print(type(x))
+        for x in myresult:
+          print(type(x))
 
     mycursor.close()
 
 except db_connector.Error as error:
-        print("Failed to insert into MySQL table {}".format(error))
-
+    print("Error al ejecutar la(s) operación(es) {} sobre la base de datos:\n {}".format(operations,error))
+    
 finally:
     if (mydb.is_connected()):
         mydb.close()
-        print("MySQL connection is closed")
+        print("La conexión ha sido cerrada exitosamente")
